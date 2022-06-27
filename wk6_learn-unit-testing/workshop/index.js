4209bb2c3ac88f3866a982bf314a96475da78ec9
Abbbyccc <abbychien@hotmail.com>
2022-06-27 13:37:28 +0100

    unit testing

--- wk6_learn-unit-testing/workshop/index.js
+++ wk6_learn-unit-testing/workshop/index.js
@@ -9,3 +9,14 @@ const searchParamsToObject = (str) => {
   return Object.fromEntries(param)
 }
 
+const isLeapYear = (yr) => {
+  if (yr < 0) {
+    return 'Please enter a positive number'
+  } else if (typeof yr !== 'number') {
+    return 'Please enter type number only'
+  } else if (yr % 100 == 0) {
+    return false
+  } else if (yr % 4 === 0 || yr % 400 === 0) {
+    return true
+  }
+}
--- wk6_learn-unit-testing/workshop/index.test.js
+++ wk6_learn-unit-testing/workshop/index.test.js
@@ -10,3 +10,15 @@ test("searchParamsToObject should return an Object from the given string", () =>
     equal(param.name, expect.name)
     equal(param.email, expect.email)
 })
+
+test("isLeapYear should return if input is leap year", () => {
+    equal(isLeapYear("hi"), "Please enter a positive number", "Input must be a number");
+    equal(isLeapYear(-1500), "Please enter type number only", "Input must be positive");
+    equal(isLeapYear(1900), false, "1900 should not be a leap year");
+});
+
+test("isLeapYear should return if input is leap year", () => {
+    equal(isLeapYear("hi_Abby"), "Please enter a positive number", "Input must be a number");
+    equal(isLeapYear(-2000), "Please enter type number only", "Input must be positive");
+    equal(isLeapYear(2020), true, "2020 should be a leap year");
+});
